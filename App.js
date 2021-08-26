9554c0e5cf12d3cdd712be76f8bc0c95fe712ac3
parthkanani93 <parthkanani8490@gmail.com>
2021-08-26 16:38:38 +0530

    Make a Demo of codepush

---
+++ App.js
@@ -0,0 +1,125 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState, useEffect} from 'react';
+import type {Node} from 'react';
+import * as tf from '@tensorflow/tfjs';
+// import '@tensorflow/tfjs-react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import withCodePush from './codepush';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [isTfReady, setIsTfReady] = useState(false);
+
+  console.log('Tf is ready======>', isTfReady);
+  // async componentDidMount() {
+  //   // Wait for tf to be ready.
+  //   await tf.ready();
+  //   // Signal to the app that tensorflow.js can now be used.
+  //   this.setState({
+  //     isTfReady: true,
+  //   });
+  // }
+  useEffect(() => {
+    const tfReady = async () => {
+      await tf.ready();
+      setIsTfReady(true);
+    };
+    tfReady();
+  }, []);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Testing Codepush">
+            <Text style={styles.highlight}>You are Right Bro</Text>
+            <Text>Hello Manoj</Text>
+            <Text>Hello Parth</Text>
+            <Text>Hello testingggggg</Text>
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default withCodePush(App);
